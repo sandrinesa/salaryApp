f368883e3c4222308547d8a8e627bdd2f9e02584
sandrinesa <58148915+sandrinesa@users.noreply.github.com>
2019-11-24 23:48:44 +0200

    Add files via upload

---
+++ salariesApp/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace SalariesApp
+{
+    public class Calculator
+    {
+        public int employee_id;
+        public string employee_name;
+        public string employee_position;
+        public string employee_gender;
+
+        public double calculateSalary(double grossSalary)
+        {
+            double salary;
+            salary = grossSalary - (((grossSalary * 30) / 100) + ((grossSalary * 5) / 100) + ((grossSalary * 3) / 100));
+            return salary;
+        }
+
+        public double calculateSalary(double rate, double hour)
+        {
+            double salary;
+            salary=(rate*hour)-((((rate*hour)*5)/100)+(((rate*hour)*3)/100));
+            return salary;
+        }
+
+        public int calculateSalary(int grossSalary)
+        {
+            int salary;
+            salary = grossSalary - ((grossSalary * 30) / 100);
+            return salary;
+        }
+    }
+}
